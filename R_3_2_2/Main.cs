02bf75d2b9abbc3b64fc4103e5f0446124650d4d
Дмитрий Изох <izokh_dv@mail.ru>
2023-01-19 23:40:20 +0300

    Добавьте файлы проекта.

---
+++ R_3_2_2/Main.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R_3_2_2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            List<FamilyInstance> fInstances = new FilteredElementCollector(doc, doc.ActiveView.Id)
+            .OfCategory(BuiltInCategory.OST_PipeCurves)
+            .WhereElementIsNotElementType()
+            .Cast<FamilyInstance>()
+            .ToList();
+
+            TaskDialog.Show("Количество труб", fInstances.Count.ToString());
+            return Result.Succeeded;
+        }
+    }
+}
